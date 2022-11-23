c4810f79b85a7f417f61f47ab00a07c3fe129c39
봄수 <37897508+GoBeromsu@users.noreply.github.com>
2022-11-23 20:13:57 +0900

    Feat : db with connection

---
+++ routes/mysql-db.js
@@ -0,0 +1,16 @@
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "berom",
+    password: "1234",
+    database: "test", // 연결할 데이터베이스
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Database is Connected");
+});
+
+module.exports = connection;
